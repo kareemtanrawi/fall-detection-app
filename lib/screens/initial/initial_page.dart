7ef4e027324c5b3be07791802c81b481d6b7b6f3
Your Name <kareem01229526319@gmail.com>
2024-01-24 10:05:15 +0200

    add image and constants file
    Signed-off-by: Your Name <kareem01229526319@gmail.com>

---
+++ lib/screens/initial/initial_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InitialPage extends StatelessWidget {
+  const InitialPage({super.key});
+  static String id = 'InitialPage';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
